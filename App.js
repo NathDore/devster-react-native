1bae0f056445fb4faafe539fa4ae398131d74868
Nath <120222018+NathDore@users.noreply.github.com>
2024-01-23 12:55:19 -0500

    Creating my bottom-tab-navigator using react-navigation/bottom-tabs and Screens (Profile, Chat, Feed).

--- App.js
+++ App.js
@@ -3,13 +3,10 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthProvider from './src/context/AuthProvider';
 import MainStack from './src/Navigation/stack/MainNavigation';
 
-const Stack = createNativeStackNavigator();
-
 export default function App() {
   return (
     <AuthProvider>
       <MainStack />
     </AuthProvider>
-
   );
 }
--- src/Navigation/stack/AppNavigation.js
+++ src/Navigation/stack/AppNavigation.js
@@ -1,9 +1,17 @@
 import { View, Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ProfileScreen from "../../Features/App/Screens/ProfileScreen";
+import ChatScreen from "../../Features/App/Screens/ChatScreen";
+import FeedScreen from "../../Features/App/Screens/FeedScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function AppStack() {
     return (
-        <View>
-            <Text>App Stack .</Text>
-        </View>
+        <Tab.Navigator>
+            <Tab.Screen name="Feed" component={FeedScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen name="Chat" component={ChatScreen} />
+        </Tab.Navigator>
     );
 }
