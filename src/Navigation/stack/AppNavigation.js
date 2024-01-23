31b247e5d19a5bac320e3c429a4b9930f742033b
Nath <120222018+NathDore@users.noreply.github.com>
2024-01-23 16:50:36 -0500

    Styling the icon of the tab bar. Giving color and fontAwesome icons.

--- src/Navigation/stack/AppNavigation.js
+++ src/Navigation/stack/AppNavigation.js
@@ -3,15 +3,53 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileScreen from "../../Features/App/Screens/ProfileScreen";
 import ChatScreen from "../../Features/App/Screens/ChatScreen";
 import FeedScreen from "../../Features/App/Screens/FeedScreen";
+import AwesomeIcon from "react-native-vector-icons/FontAwesome";
 
 const Tab = createBottomTabNavigator();
 
 export default function AppStack() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Feed" component={FeedScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
-            <Tab.Screen name="Chat" component={ChatScreen} />
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveBackgroundColor: "lightgray",
+            tabBarStyle: {
+                height: 55,
+            }
+        }}>
+            <Tab.Screen
+                name="Feed"
+                component={FeedScreen}
+                options={
+                    {
+                        tabBarIcon: () => (
+                            <AwesomeIcon name="feed" color={"black"} size={35} />
+                        ),
+                    }
+                }
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={
+                    {
+                        tabBarIcon: () => (
+                            <AwesomeIcon name="user" color={"black"} size={35} />
+                        ),
+                    }
+                }
+            />
+            <Tab.Screen
+                name="Chat"
+                component={ChatScreen}
+                options={
+                    {
+                        tabBarIcon: () => (
+                            <AwesomeIcon name="comment" color={"black"} size={35} />
+                        ),
+                    }
+                }
+            />
         </Tab.Navigator>
     );
 }
