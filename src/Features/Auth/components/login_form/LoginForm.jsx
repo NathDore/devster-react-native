ace450804e919a6c1af2fb4502cc1f0ee98b328e
Nath <120222018+NathDore@users.noreply.github.com>
2024-01-23 12:05:37 -0500

    Creating login form using react-hook-form.

--- src/Features/Auth/components/login_form/LoginForm.jsx
+++ src/Features/Auth/components/login_form/LoginForm.jsx
@@ -1,22 +1,90 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Pressable } from 'react-native'
 import React from 'react'
 import { Icon } from 'react-native-elements';
 import { useAuthContext } from '../../../../context/AuthProvider';
+import { FORM_STYLESHEET } from '../Form_Styles/style';
+import { Controller, useForm } from 'react-hook-form';
 
 const LoginForm = () => {
     const { openLoginForm } = useAuthContext();
+    const { control, handleSubmit, formState: { isValid } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ width: "70%", backgroundColor: "white", padding: "5%" }}>
+
+            {/* Modal */}
+            <View style={FORM_STYLESHEET.modal}>
+
                 <TouchableOpacity
-                    style={{ borderWidth: 1, width: 25 }}
+                    style={FORM_STYLESHEET.closeIcon}
                     onPress={openLoginForm}
                 >
                     <Icon name="close" type="fontAwesome" color={"black"} />
                 </TouchableOpacity>
 
-                <Text style={{ color: "black" }}>LoginForm</Text>
+                {/* Form */}
+                <View style={FORM_STYLESHEET.form}>
+                    {/* Title */}
+                    <Text style={FORM_STYLESHEET.title}>Login in.</Text>
+
+                    {/* Email Field */}
+                    <Controller
+                        control={control}
+                        render={({ field, fieldState }) => (
+                            <View style={FORM_STYLESHEET.field}>
+                                <Text style={FORM_STYLESHEET.labelText}>Email:</Text>
+                                <TextInput
+                                    onChangeText={field.onChange}
+                                    onBlur={field.onBlur}
+                                    value={field.value}
+                                    style={FORM_STYLESHEET.userInput}
+                                />
+                                {fieldState.error && <Text style={{ color: 'red' }}>{fieldState.error.message}</Text>}
+                            </View>
+                        )}
+                        name="name"
+                        rules={{
+                            required: 'Name is required'
+                        }}
+                    />
+
+                    {/* Password field */}
+                    <Controller
+                        control={control}
+                        render={({ field, fieldState }) => (
+                            <View style={FORM_STYLESHEET.field}>
+                                <Text style={FORM_STYLESHEET.labelText}>Password:</Text>
+                                <TextInput
+                                    onChangeText={field.onChange}
+                                    onBlur={field.onBlur}
+                                    value={field.value}
+                                    secureTextEntry
+                                    style={FORM_STYLESHEET.userInput}
+                                />
+                                {fieldState.error && <Text style={{ color: 'red' }}>{fieldState.error.message}</Text>}
+                            </View>
+                        )}
+                        name="password"
+                        rules={{
+                            required: 'Password is required',
+                        }}
+                    />
+
+                    {/* Submit button */}
+                    {
+                        isValid ? <Pressable style={FORM_STYLESHEET.validButton} onPress={handleSubmit(onSubmit)}>
+                            <Text style={FORM_STYLESHEET.textButton}>Login</Text>
+                        </Pressable> : <View style={FORM_STYLESHEET.invalidButton}>
+                            <Text style={FORM_STYLESHEET.textButton}>Login</Text>
+                        </View>
+                    }
+
+                </View>
+
             </View>
 
         </View>
