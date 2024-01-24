d14bd9aea463c65bce6cc8a750d8726fd7a0c7a2
Nath <120222018+NathDore@users.noreply.github.com>
2024-01-24 17:29:32 -0500

    Creation du component Post avec styles. Affiche les posts dans le feed screen avec une flatlist.

--- src/Features/App/Screens/FeedScreen.jsx
+++ src/Features/App/Screens/FeedScreen.jsx
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, FlatList } from 'react-native'
 import React from 'react'
+import Post from '../../../UI/Feed-Post/Post'
+import { dumData } from '../../../data/dummyData'
 
 const FeedScreen = () => {
+    const renderItem = ({ item }) => (
+        <Post
+            profileImg={item.profileImg}
+            name={item.username}
+            timestamps={item.timeStamps}
+            post={item.post}
+            likes={item.likes}
+            comments={item.comments}
+        />
+    );
+
+
     return (
-        <View>
-            <Text>FeedScreen</Text>
+        <View style={{
+            flex: 1,
+            backgroundColor: "#202124",
+            alignItems: "center",
+        }}>
+            <FlatList
+                data={dumData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()} // Assurez-vous d'avoir une propriété unique "id" dans vos données
+            />
         </View>
     )
 }
--- src/UI/Feed-Post/Post.jsx
+++ src/UI/Feed-Post/Post.jsx
@@ -1,10 +1,68 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable } from 'react-native'
+import { Avatar } from 'react-native-elements';
+import AwesomeIcon from "react-native-vector-icons/FontAwesome";
+import AwesomeIcon5 from 'react-native-vector-icons/FontAwesome5';
+import React, { useState } from 'react';
+import { POST_STYLESHEET } from './style';
 
-const Post = ({ profileImg, timestamps, name }) => {
+const Post = ({ profileImg, timestamps, name, post, likes, comments }) => {
+    const [isLike, setIslike] = useState(false);
+
+    const handleLike = () => {
+        setIslike(prev => !prev);
+    }
+
     return (
-        <View>
-            <Text>Post</Text>
+        <View style={{
+            borderTopWidth: 0.5,
+            borderBottomWidth: 0.5,
+            borderColor: "#d1d0d059",
+            width: "100%",
+            paddingHorizontal: "2%",
+            paddingVertical: "1%",
+        }}>
+            {/* Info Container */}
+            <View style={POST_STYLESHEET.info_container}>
+                {/* Profile picture */}
+                <Avatar
+                    size={20}
+                    rounded
+                    source={profileImg}
+                />
+
+                {/* UserName */}
+                <Text style={POST_STYLESHEET.username}>{name}</Text>
+
+                {/* Timestamp */}
+                <Text style={POST_STYLESHEET.timestamp}>{timestamps}</Text>
+            </View>
+
+            {/* Post Container */}
+            <View style={POST_STYLESHEET.post_container}>
+                {/* Post text */}
+                <Text style={POST_STYLESHEET.post_text}>{post}</Text>
+            </View>
+
+            {/* Reaction Section */}
+            <View style={POST_STYLESHEET.reaction_container}>
+
+                {/* Like button */}
+                {
+                    isLike ? <Pressable onPress={handleLike}>
+                        <AwesomeIcon name="heart" color={"red"} size={15} style={POST_STYLESHEET.like_icon} />
+                    </Pressable> : <Pressable onPress={handleLike}>
+                        <AwesomeIcon5 name="heart" color={"lightgrey"} size={15} solid={false} style={POST_STYLESHEET.like_icon} />
+                    </Pressable>
+
+                }
+
+                {/* Comment button */}
+                <Pressable>
+                    <AwesomeIcon5 name="comment" color={"lightgrey"} size={15} solid={false} />
+                </Pressable>
+
+            </View>
+
         </View>
     )
 }
